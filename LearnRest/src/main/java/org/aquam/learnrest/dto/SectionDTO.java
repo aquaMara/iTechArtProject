79756890d96dba8303683e9cbbf07413abc569bd
Mara <aquamarina.na@gmail.com>
2022-04-05 19:10:17 +0300

    fix(section): add equals-method

--- LearnRest/src/main/java/org/aquam/learnrest/dto/SectionDTO.java
+++ LearnRest/src/main/java/org/aquam/learnrest/dto/SectionDTO.java
@@ -3,6 +3,7 @@ package org.aquam.learnrest.dto;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -17,4 +18,16 @@ public class SectionDTO {
 
     private Long subjectId;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SectionDTO that = (SectionDTO) o;
+        return Objects.equals(sectionName, that.sectionName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sectionName);
+    }
 }
